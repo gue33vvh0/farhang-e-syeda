3aec48f2099a6b90167cf4e092f308f439c3f7ba
gue33vvh0 <122175218+gue33vvh0@users.noreply.github.com>
2023-07-03 15:49:30 -0400

    test search return functionality
    drop the toLowercase() bit
    
    test using urdu key/value pairs instead of english
    
    test urdu words
    
    make search result font much larger & return nastaliq
    
    retry using a dictionary.json file, make font even bigger
    
    comment out dictionaryData var
    
    spacing issues?
    
    unicode helper script to reliably store words in dictionary.json

---
+++ unicode_convert.py
@@ -0,0 +1,30 @@
+import json
+
+def convert_to_unicode(text):
+    unicode_text = text.encode('unicode-escape').decode()
+    return unicode_text
+
+# Prompt the user for the word and meaning in Urdu
+word = input("Enter the word in Urdu: ")
+meaning = input("Enter the meaning in Urdu: ")
+
+# Convert the word and meaning to Unicode
+unicode_word = convert_to_unicode(word)
+unicode_meaning = convert_to_unicode(meaning)
+
+# Load existing entries from the JSON file
+with open("dictionary.json", "r", encoding="utf-8") as file:
+    entries = json.load(file)
+
+# Create a new entry object
+entry = {"word": unicode_word, "meaning": unicode_meaning}
+
+# Append the new entry to the existing entries
+entries.append(entry)
+
+# Write the updated entries back to the JSON file
+with open("dictionary.json", "w", encoding="utf-8") as file:
+    json.dump(entries, file, ensure_ascii=False, indent=4)
+
+print("Entry added successfully!")
+
