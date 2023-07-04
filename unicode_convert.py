f5ebecfe84b0b054da33e7896098a68d48a8a813
Asad <asad.k.khan@oracle.com>
2023-07-03 22:03:29 -0400

    request for source when entering words into JSON
    meaningElement was incorrectly defined as wordElement for meaningElement var

--- unicode_convert.py
+++ unicode_convert.py
@@ -7,17 +7,19 @@ def convert_to_unicode(text):
 # Prompt the user for the word and meaning in Urdu
 word = input("Enter the word in Urdu: ")
 meaning = input("Enter the meaning in Urdu: ")
+source = input("Enter the source: ")
 
 # Convert the word and meaning to Unicode
 unicode_word = convert_to_unicode(word)
 unicode_meaning = convert_to_unicode(meaning)
+source = input("Enter the source: ")
 
 # Load existing entries from the JSON file
 with open("dictionary.json", "r", encoding="utf-8") as file:
     entries = json.load(file)
 
 # Create a new entry object
-entry = {"word": unicode_word, "meaning": unicode_meaning}
+entry = {"word": unicode_word, "meaning": unicode_meaning, "source": unicode_source}
 
 # Append the new entry to the existing entries
 entries.append(entry)
